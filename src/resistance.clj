0e3f63b30d8f55610642b64f96fc97a06e7a062f
gssiyankai <syk.greg@gmail.com>
2013-12-12 23:10:54 +0100

    add dictionary

--- src/resistance.clj
+++ src/resistance.clj
@@ -1,5 +1,15 @@
 (ns resistance)
 
+(defn alphabet
+  {\A [:dot :dash] \B [:dash :dot :dot :dot] \C [:dash :dot :dash :dot] \D [:dash :dot :dot]
+   \E [:dot] \F [:dot :dot :dash :dot] \G [:dash :dash :dot] \H [:dot :dot :dot :dot]
+   \I [:dot :dot] \J [:dot :dash :dash :dash] \K [:dash :dot :dash] \L [:dot :dash :dot :dot]
+   \M [:dash :dash] \N [:dash :dot] \O [:dash :dash :dash] \P [:dot :dash :dash :dot]
+   \Q [:dash :dash :dot :dash] \R [:dot :dash :dot] \S [:dot :dot :dot] \T [:dash]
+   \U [:dot :dot :dash] \V [:dot :dot :dot :dash] \W [:dot :dash :dash] \X [:dash :dot :dot :dash]
+   \Y [:dash :dot :dash :dash] \Z [:dash :dash :dot :dot]}
+)
+
 (defn number-of-messages
   [morse dictionary]
   nil
