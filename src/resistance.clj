12c5bc06c248844e1c4cb44855c806d491fb4c66
gssiyankai <syk.greg@gmail.com>
2013-12-13 21:52:31 +0100

    detection of word

--- src/resistance.clj
+++ src/resistance.clj
@@ -17,16 +17,17 @@
 (defn number-of-messages
   ([morse dictionary]
     (let [dictionary-morse (set (dictionary-morse dictionary))]
-      (number-of-messages morse "" dictionary-morse)))
-  ([morse preceding-morse dictionary]
+      (number-of-messages morse "" dictionary-morse nil)))
+  ([morse preceding-morse dictionary word]
     (let [current-morse (str preceding-morse (first morse))
           rest-morse (rest morse)
-          stop (empty? rest-morse)]
-      (->> (map #(if (.startsWith % current-morse) % nil) dictionary)
+          stop (empty? rest-morse)
+          words (if (nil? word) dictionary [word])]
+      (->> (map #(if (.startsWith % current-morse) % nil) words)
            (filter #(not (nil? %)))
            (map #(if (= current-morse %)
-                        (if stop 1 (inc (number-of-messages rest-morse "" [%])))
-                        (if stop 0 (number-of-messages rest-morse current-morse [%]))))
+                        (if stop 1 (number-of-messages rest-morse "" dictionary nil))
+                        (if stop 0 (number-of-messages rest-morse current-morse dictionary %))))
            (flatten)
            (reduce +))))
 )
