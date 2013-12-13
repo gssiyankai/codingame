80590a8c05e929d448f6c7961b3c350ad9837cd7
gssiyankai <syk.greg@gmail.com>
2013-12-13 21:39:44 +0100

    simple brute force computation

--- src/resistance.clj
+++ src/resistance.clj
@@ -15,6 +15,18 @@
 )
 
 (defn number-of-messages
-  [morse dictionary]
-  nil
+  ([morse dictionary]
+    (let [dictionary-morse (set (dictionary-morse dictionary))]
+      (number-of-messages morse "" dictionary-morse)))
+  ([morse preceding-morse dictionary]
+    (let [current-morse (str preceding-morse (first morse))
+          rest-morse (rest morse)
+          stop (empty? rest-morse)]
+      (->> (map #(if (.startsWith % current-morse) % nil) dictionary)
+           (filter #(not (nil? %)))
+           (map #(if (= current-morse %)
+                        (if stop 1 (inc (number-of-messages rest-morse "" [%])))
+                        (if stop 0 (number-of-messages rest-morse current-morse [%]))))
+           (flatten)
+           (reduce +))))
 )
