a7c4509b5c25311f7478b534c9db8c0e0df6cfa1
gssiyankai <syk.greg@gmail.com>
2013-12-13 23:11:27 +0100

    memoize

--- src/resistance.clj
+++ src/resistance.clj
@@ -14,20 +14,18 @@
   (map (fn [word] (apply str (map #((alphabet) %) word))) dictionary)
 )
 
-(defn number-of-messages
-  ([morse dictionary]
-    (let [dictionary-morse (set (dictionary-morse dictionary))]
-      (number-of-messages morse "" dictionary-morse nil)))
-  ([morse preceding-morse dictionary word]
-    (let [current-morse (str preceding-morse (first morse))
-          rest-morse (rest morse)
-          stop (empty? rest-morse)
-          words (if (nil? word) dictionary [word])]
-      (->> (map #(if (.startsWith % current-morse) % nil) words)
-           (filter #(not (nil? %)))
-           (map #(if (= current-morse %)
-                        (if stop 1 (number-of-messages rest-morse "" dictionary nil))
-                        (if stop 0 (number-of-messages rest-morse current-morse dictionary %))))
-           (flatten)
+(def morse-messages
+  (memoize
+    (fn [morse dictionary]
+      (->> (map #(cond (= morse %) 1
+                       (.startsWith morse %) (morse-messages (subs morse (count %)) dictionary)
+                       :else 0)
+              dictionary)
            (reduce +))))
+)
+
+(defn number-of-messages
+  [morse dictionary]
+    (let [dictionary-morse (set (dictionary-morse dictionary))]
+      (morse-messages morse dictionary-morse))
 )
