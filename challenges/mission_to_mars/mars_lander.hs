92fb4098a4cbd30b4f6cc2ae0617da2ac052297e
gssiyankai <syk.greg@gmail.com>
2014-01-26 19:25:21 +0100

    Mars Lander: question 1

--- challenges/mission_to_mars/mars_lander.hs
+++ challenges/mission_to_mars/mars_lander.hs
@@ -1,53 +1,57 @@
+import qualified Data.Set as S
+import qualified Data.Map as M
+import Control.Applicative
+import Control.Monad
 import System.IO
+
+data Position = Position 
+    {
+        px :: Int,
+        py :: Int
+    }
+
+data MarsLander = MarsLander
+    {
+        x :: Int,
+        y :: Int,
+        hs :: Int,
+        vs :: Int,
+        f :: Int,
+        r :: Int,
+        p :: Int
+    }
 
 main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering -- DO NOT REMOVE
     
     -- Read init information from standard input, if any
-    line <- getLine
-    info <- readInitInfo (read line :: Int)
+    n <- read <$> getLine
+    positions <- replicateM n ((\ (x:y:_) -> Position x y) . map read . words <$> getLine)
     
-    loop $ positions info
+    loop positions
 
-readInitInfo :: Int -> IO (String)
-readInitInfo 0 = return ""
-readInitInfo n = do
-    line <- getLine
-    nextLine <- readInitInfo (n-1)
-    return (line ++ nextLine)
-
-convert :: String -> [Int]
-convert line = map (\ x -> read x :: Int) $ words line
-
-positions :: String -> [(Int,Int)]
-positions info = positions' $ convert info
-    where positions' (x:y:ps) = (x,y) : positions' ps
-
-params :: String -> IO ([Int])
-params line = return (convert line)
-
-rotation :: [Int] -> Int
+rotation :: MarsLander -> Int
 rotation _ = 0
 
-power :: [Int] -> Int
-power (x:y:hs:vs:f:r:p)
-    | abs vs >= 40  = 4
+power :: MarsLander -> Int
+power ml
+    | abs (vs ml) >= 40 = 4
     | otherwise = 0
     
 
-loop :: [(Int, Int)] -> IO ()
+loop :: [Position] -> IO ()
 loop positions = do
     -- Read information from standard input
-    line <- getLine
+    marsLander <- (\ (x:y:hs:vs:f:r:p:_) -> MarsLander x y hs vs f r p) . map read .words <$> getLine
     
     -- Compute logic here
-    params <- params line
+    
     
     -- hPutStrLn stderr "Debug messages..."
     
     -- Write action to standard output
-    putStrLn $ show (rotation params) ++ " " ++ show (power params)
+    putStrLn $ show (rotation marsLander) ++ " " ++ show (power marsLander)
     
     loop positions
     
