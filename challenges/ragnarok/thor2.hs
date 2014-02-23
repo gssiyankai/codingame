68862fdfb38a4b5ca194ac19a1d1123a831f25f6
gssiyankai <syk.greg@gmail.com>
2014-02-23 12:49:28 +0100

    ragnarok

--- challenges/ragnarok/thor2.hs
+++ challenges/ragnarok/thor2.hs
@@ -10,61 +10,56 @@ main = do
     
     loop thor_position
 
+distance :: [Int] -> [Int] -> Int
+distance (tx:ty:_) (gx:gy:_) = square (tx-gx) + square (ty-gy)
+    where square x = x * x
 
 nearestGiant :: [Int] -> [[Int]] -> [Int]
-nearestGiant thor_position@(tx:ty:_) giants_positions = head (sortBy sortGiantsByDistance giants_positions)
-    where sortGiantsByDistance (g1x:g1y:_) (g2x:g2y:_) 
-                | min_delta1 < min_delta2 = LT
-                | min_delta1 > min_delta2 = GT
-                | max_delta1 < max_delta2 = LT
-                | max_delta1 > max_delta2 = GT
-                | otherwise               = EQ
-                where max_delta1 = max delta1_x delta1_y
-                      max_delta2 = max delta2_x delta2_y
-                      min_delta1 = min delta1_x delta1_y
-                      min_delta2 = min delta2_x delta2_y
-                      delta1_x = abs(g1x-tx)
-                      delta1_y = abs(g1y-ty)
-                      delta2_x = abs(g2x-tx)
-                      delta2_y = abs(g2y-ty)
+nearestGiant thor_position@(tx:ty:_) giants_positions = 
+    snd $ head $ sort $ map (\giant_position -> (distance thor_position giant_position, giant_position)) giants_positions
                       
-
 isGiantInStrikeRange :: [Int] -> [Int] -> Bool
 isGiantInStrikeRange (tx:ty:_) (gx:gy:_)
     | abs (tx - gx) > 5 = False
     | abs (ty - gy) > 5 = False
     | otherwise         = True
     
-areAllGiansInStrikeRange :: [Int] -> [[Int]] -> Bool
-areAllGiansInStrikeRange thor_position giants_positions = and $ map (\giant_position -> isGiantInStrikeRange thor_position giant_position) giants_positions
+areAllGiantsInStrikeRange :: [Int] -> [[Int]] -> Bool
+areAllGiantsInStrikeRange thor_position giants_positions = and $ map (\giant_position -> isGiantInStrikeRange thor_position giant_position) giants_positions
+
+isThorOutOfGiantRange :: [Int] -> [Int] -> Bool
+isThorOutOfGiantRange (tx:ty:_) (gx:gy:_) = abs(tx-gx) > 1 || abs(ty-gy)>1
+
+isThorOutOfAllGiantsRange :: [Int] -> [[Int]] -> Bool
+isThorOutOfAllGiantsRange thor_position giants_positions = and $ map (\giant_position -> isThorOutOfGiantRange thor_position giant_position) giants_positions
 
 computeMove :: [Int] -> [[Int]] -> String
-computeMove thor_position@(tx:ty:_) giants_positions = move
+computeMove thor_position@(tx:ty:_) giants_positions = move 
     where move = vertical_move ++ horizontal_move
-          vertical_move | delta_y < 0 = "S"
-                        | delta_y > 0 = "N"
+          vertical_move | ty+1<18 && delta_y < 0 = "S"
+                        | ty-1>=0 && delta_y > 0 = "N"
                         | otherwise   = ""
-          delta_y = ngy - ty
-          horizontal_move | delta_x < 0 = "E"
-                          | delta_x > 0 = "W"
+          delta_y = ngy-ty
+          horizontal_move | tx+1<40 && delta_x < 0 = "E"
+                          | tx-1>=0 && delta_x > 0 = "W"
                           | otherwise   = ""
-          delta_x = ngx - tx
-          nearest_giant = nearestGiant thor_position giants_positions
-          ngx = nearest_giant !! 0
-          ngy = nearest_giant !! 1
+          delta_x = ngx-tx
+          [ngx, ngy] = nearestGiant thor_position giants_positions
     
 computeAction :: [Int] -> Int -> [[Int]] -> String
 computeAction thor_position strikes giants_positions
-    | shouldMove = move
+    | shouldMove && isMoveValid = move
     | shouldStrike = "STRIKE"
     | otherwise    = "WAIT"
-    where shouldMove = hasNotEnoughStrikes && cannotStrikeAllGiants && isThorNextPositionInBounds
+    where shouldMove = hasNotEnoughStrikes && cannotStrikeAllGiants
           hasNotEnoughStrikes = strikes <= length giants_positions
-          move = computeMove thor_position giants_positions
+          cannotStrikeAllGiants = not $ areAllGiantsInStrikeRange thor_position giants_positions
           next_thor_position = computeNextThorPosition thor_position move
           isThorNextPositionInBounds = next_thor_position !! 0 >= 0 && next_thor_position !! 0 < 40 && 
-                                       next_thor_position !! 1 >= 0 && next_thor_position !! 1 < 100
-          cannotStrikeAllGiants = not $ areAllGiansInStrikeRange thor_position giants_positions
+                                       next_thor_position !! 1 >= 0 && next_thor_position !! 1 < 18
+          isThorNextPositionOutOfGiantsRange = isThorOutOfAllGiantsRange next_thor_position giants_positions
+          isMoveValid = isThorNextPositionInBounds && isThorNextPositionOutOfGiantsRange
+          move = computeMove thor_position giants_positions
           shouldStrike = isGiantInStrikeRange thor_position (nearestGiant thor_position giants_positions)
 
 computeNextThorPosition :: [Int] -> String -> [Int]
@@ -88,8 +83,11 @@ loop thor_position = do
     lines <- replicateM nb_giants getLine
     let giants_positions = map (\line -> map (\coord -> read coord) line)
                             $ map words lines
+    hPutStrLn stderr $ show thor_position
     hPutStrLn stderr $ show strikes
     hPutStrLn stderr $ show giants_positions
+    hPutStrLn stderr $ show $ nearestGiant thor_position giants_positions
+    hPutStrLn stderr $ show $ computeMove thor_position giants_positions
     
 
     -- Compute logic here
