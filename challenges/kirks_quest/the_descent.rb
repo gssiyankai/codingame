1192d5165e9484162662aa647c280f00f3c0b859
gssiyankai <syk.greg@gmail.com>
2014-03-23 09:15:24 +0100

    Kirk - The descent

---
+++ challenges/kirks_quest/the_descent.rb
@@ -0,0 +1,25 @@
+STDOUT.sync = true # DO NOT REMOVE
+
+# Read init information from standard input, if any
+
+loop do
+    # Read information from standard input
+    s = STDIN.gets.split(" ").map(&:to_i)
+    ms = (0..7).map { |i|
+                    [i, STDIN.gets.to_i]
+                }
+
+    # Compute logic here
+    hm = ms.max_by { |i, h| 
+                h
+            }
+    action = "HOLD"
+    if hm[0]==s[0]
+        action = "FIRE"
+    end
+
+    # STDERR.puts "Debug messages..."
+
+    # Write action to standard output
+    puts action
+end
