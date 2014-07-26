145dd1f210747e2b1f0a72b96220589036bf6944
gssiyankai <syk.greg@gmail.com>
2014-07-26 20:50:42 +0200

    Shadows of the knight - level 2

--- challenges/shadows_of_the_knight/Batman2.py
+++ challenges/shadows_of_the_knight/Batman2.py
@@ -18,42 +18,44 @@ YS = range(0,H)
 
 bomb_x_found = False
 
+def move(Z2, Z1, ZS, ZMAX, code):
+    print(Z1, file=sys.stderr)
+    print(Z2, file=sys.stderr)
+    if Z1!=Z2:
+        if code == "WARMER":
+            ZS = [z for z in ZS if abs(z-Z2) < abs(z-Z1)]
+        elif code == "COLDER":
+            ZS = [z for z in ZS if abs(z-Z1) < abs(z-Z2)]
+        elif code == "SAME":
+            print(ZS, file=sys.stderr)
+            ZS = [z for z in ZS if Z1 <= z and z <= Z2]
+            ZS = [ZS[len(ZS)//2]]
+            print(ZS, file=sys.stderr)
+    
+    Z1 = Z2
+    if len(ZS)==1:
+        Z2 = ZS[0]
+    elif Z2 <= ZS[0]:
+        Z2 = min(ZMAX-1, ZS[-1] + ZS[0] - Z2)
+    elif Z2 >= ZS[-1]:
+        Z2 = max(0, ZS[0] - (Z2 - ZS[-1]))
+    else:
+        Z2 = ZS[0]
+
+    return Z2, Z1, ZS
+
 while 1:
     # Read information from standard input
     code = input()
     print("code=%s" % code, file=sys.stderr)
 
     # Compute logic here
-    if code == "UNKNOWN":
-        X2 = XS[len(XS)//2]
-    elif code == "WARMER":
-        if not bomb_x_found:
-            XS = [x for x in XS if abs(x-X2) < abs(x-X1)]
-            X1 = X2
-            X2 = XS[len(XS)//2]
-        else:
-            YS = [y for y in YS if abs(y-Y2) < abs(y-Y1)]
-            Y1 = Y2
-            Y2 = YS[len(YS)//2]
-    elif code == "COLDER":
-        if not bomb_x_found:
-            XS = [x for x in XS if abs(x-X1) < abs(x-X2)]
-            X1 = X2
-            X2 = XS[len(XS)//2]
-        else:
-            YS = [y for y in YS if abs(y-Y1) < abs(y-Y2)]
-            Y1 = Y2
-            Y2 = YS[len(YS)//2]
-    elif code == "SAME":
-        if not bomb_x_found:
+    if not bomb_x_found:
+        X2, X1, XS = move(X2, X1, XS, W, code)
+        if len(XS)==1:
             bomb_x_found = True
-            if len(XS) > 1:
-                XS = [x for x in XS if x != X2]
-            Y2 = YS[len(YS)//2]
-        else:
-            if len(YS) > 1:
-                YS = [y for y in YS if y != Y2]
-                Y2 = YS[len(YS)//2]
+    else:
+        Y2, Y1, YS = move(Y2, Y1, YS, H, code)
 
     # Write action to standard output
     print("%s %s" % (X2,Y2))
