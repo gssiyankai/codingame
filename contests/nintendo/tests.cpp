36c637deac4b725a14540d55bc488cce0fee7d5f
gssiyankai <syk.greg@gmail.com>
2015-01-17 20:11:52 +0100

    Add encode tests

--- contests/nintendo/tests.cpp
+++ contests/nintendo/tests.cpp
@@ -177,10 +177,16 @@ TEST(MessageTest, Parse)
 
 TEST(EncoderTest, Encode)
 {
-    Message m1 = Message::parse("00000083 000000e5");
-    ASSERT_EQ(Message::parse("000073af 00000000"), encode(m1));
-    Message m2 = Message::parse("b0c152f9 ebf2831f");
-    ASSERT_EQ(Message::parse("46508fb7 6677e201"), encode(m2));
+    ASSERT_EQ(Message::parse("000073af 00000000"), encode(Message::parse("00000001 000073af")));
+    ASSERT_EQ(Message::parse("000073af 00000000"), encode(Message::parse("00000083 000000e5")));
+    ASSERT_EQ(Message::parse("738377c1 00000000"), encode(Message::parse("0000cd55 0000b0c5")));
+    ASSERT_EQ(Message::parse("46508fb7 6677e201"), encode(Message::parse("b0c152f9 ebf2831f")));
+    ASSERT_EQ(Message::parse("f3268b49 661859eb 0b324559 65ee6bda"),
+       encode(Message::parse("0cf5c2bf 9aba68ef c18fb79b de70eef7")));
+    ASSERT_EQ(Message::parse("a91db473 fcea8db4 f3bb434a 8dba2f16 51abc87e 92c44759 5c1a16d3 6111c6f4"),
+       encode(Message::parse("c58f4047 d73fe36a 24be2846 e2ebe432 a30d28bd bda19675 3f95d074 b6f69434")));
+    ASSERT_EQ(Message::parse("4af6fc33 39029380 465c5267 c72f6a8b 0906e6d0 ca60550f 14a5e47c 42ad10fb 4a3bb446 bb74360a 5ea02b9c 23c68553 3fade253 e270ba24 39e141ad 6c38c43d"),
+       encode(Message::parse("320a18d5 b61b13f6 1aaaa61c 0afe2a41 1a4ff107 84cc2efc 956ff31d fa595299 33749a7f 6cc9659d dc503569 ef4d0ef5 73b746c5 b8fb36d3 7616e9d6 b21251c4")));
 }
 
 int main(int argc, char **argv)
