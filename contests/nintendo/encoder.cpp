4afb2804b8756ed02f1a712540bae99e7de5047c
gssiyankai <syk.greg@gmail.com>
2015-01-19 10:35:45 +0100

    Refactor

--- contests/nintendo/encoder.cpp
+++ contests/nintendo/encoder.cpp
@@ -2,26 +2,26 @@
 
 Message encode(const Message &m)
 {
-    unsigned int size = m.size();
-    Message m1 = m.submsg(0, size / 2);
-    Message m2 = m.submsg(size / 2, size / 2).reverse();
+    const unsigned int size = m.size();
+    const Message &m1 = m.submsg(0, size / 2);
+    const Message &m2 = m.submsg(size / 2, size / 2).reverse();
 
     Message e(size);
     for (int i = size / 2 - 1; i > 0; --i)
     {
         {
-            Message sub_m1 = m1.submsg(i, size / 2 - i);
-            Message sub_m2 = m2.submsg(0, size / 2 - i);
-            Message sub_e = sub_m1 & sub_m2;
+            const Message &sub_m1 = m1.submsg(i, size / 2 - i);
+            const Message &sub_m2 = m2.submsg(0, size / 2 - i);
+            const Message &sub_e = sub_m1 & sub_m2;
             if (sub_e.cardinality() % 2 == 1)
             {
                 e.set(i);
             }
         }
         {
-            Message sub_m1 = m1.submsg(0, size / 2 - i);
-            Message sub_m2 = m2.submsg(i, size / 2 - i);
-            Message sub_e = sub_m1 & sub_m2;
+            const Message &sub_m1 = m1.submsg(0, size / 2 - i);
+            const Message &sub_m2 = m2.submsg(i, size / 2 - i);
+            const Message &sub_e = sub_m1 & sub_m2;
             if (sub_e.cardinality() % 2 == 1)
             {
                 e.set(size - i);
